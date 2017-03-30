54ab8fce93a89dcc412e979f4200d75486fe1371
JiangJinHong <497407672@qq.com>
2017-03-30 22:37:40 +0800

    17/03/30/22:37

--- 导弹拦截/导弹拦截/daodanlanjie.cpp
+++ 导弹拦截/导弹拦截/daodanlanjie.cpp
@@ -5,11 +5,15 @@ int main()
 {
 	int a[21] = { 0 }, ans[21] = { 0 }, n(1);
 	while (cin >> a[n])++n;
-	
+	n--;
+
+	for (int i = 1; i <= n; ++i)
+		ans[i] = 1;
+
 	for (int i = 1; i <= n; ++i)
 		for (int j = 1; j < i; ++j)
 			if (a[j] > a[i])ans[i] = max(ans[j] + 1, ans[i]);
-	cout << *max_element(ans + 1, ans + n + 1) << endl;
+	cout << *max_element(ans + 1, ans + n + 1 ) << endl;
 
 	for (int i = 1; i <= n; ++i)
 		ans[i] = 1;
@@ -18,5 +22,6 @@ int main()
 		for (int j = 1; j < i; ++j)
 			if (a[j] < a[i])ans[i] = max(ans[j] + 1, ans[i]);
 	cout << *max_element(ans + 1, ans + n + 1);
+	
 	return 0;
 }
